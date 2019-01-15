df76410efa4e9a1202c12d6e9638f472b6c5a3b8
Yash Khare <yashsja@gmail.com>
2019-01-15 18:13:56 +0530

    Updating my website.

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+* @Author: Venu
+* @Date:   2017-06-22 10:45:00
+* @Last Modified by:   Venu
+* @Last Modified time: 2017-08-27 12:13:16
+*/
+
+$(function(){
+	$("#about-me").typed({
+		strings: ["Student at Amrita University.","Member @amfoss.","Android Developer.","Open Source Enthusiast.","Interested in Machine Learning and AI.","Optimist | Guitarist |Techie.","Loves to Code <&#x2764>."],
+		typeSpeed: 30,
+		loop: true,
+	});
+});
