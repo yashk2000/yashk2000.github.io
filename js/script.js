016648ae9e0f2f3764257a534fab89bf37022ca0
yashk2000 <yashsja@gmail.com>
2019-11-30 10:58:00 +0530

    Minor updates

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 /*
-* @Author: Venu
+* @Author: Yash
 * @Date:   2017-06-22 10:45:00
-* @Last Modified by:   Venu
-* @Last Modified time: 2017-08-27 12:13:16
+* @Last Modified by:   Yash
+* @Last Modified time: 2019-11-30 10:50:19
 */
 
 $(function(){
 	$("#about-me").typed({
-		strings: ["Student at Amrita University.","Member @amfoss.","Android Developer.","Open Source Enthusiast.","Interested in Machine Learning and AI.","Optimist | Guitarist |Techie.","Loves to Code <&#x2764>."],
+		strings: ["Student at Amrita University.","Member and mentor @amfoss.","Computer Vision | Android Developer","Open Source Enthusiast","Interested in Computer Vision and DL", "Optimist | Guitarist |Techie.","Loves to Code <&#x2764>."],
 		typeSpeed: 30,
 		loop: true,
 	});
