47659f3cc18dd3a79eb4de7c7cf2c39217330fe1
Yash Khare <yashsja@gmail.com>
2022-01-20 14:00:14 +0530

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,53 +3,56 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "e29584293b24b8087383034e0cfff265",
+  "version.json": "b6474d93f209eda683cbb3c4e65f581b",
 "index.html": "2fb0cd66212d9bb9c72334954bde4386",
 "/": "2fb0cd66212d9bb9c72334954bde4386",
-"assets/NOTICES": "87922b1984b9bdf901948c356d61f486",
-"assets/AssetManifest.json": "8e56ba784ba977535a642b72ba332584",
+"main.dart.js": "cf6ff1b967c31a104c2b952724738936",
+"assets/AssetManifest.json": "2b1cf75d16409efef0a3586b17ce27f9",
+"assets/NOTICES": "0e559839c1f64807b152747d036ece4b",
 "assets/FontManifest.json": "ab310715c29abd30b1eb1b6c626d8ec7",
-"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"assets/assets/linkedin.png": "926e2dcf5ab4220a359867614556df68",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/assets/instagram.png": "26631a4043b14dff84180bdf51c3cacb",
+"assets/assets/github.png": "d22ee3727a7216019c3848df6eafa024",
 "assets/assets/gitlab.png": "d8dd9f3a3a1275356f9e1428b144e99f",
 "assets/assets/computervisionn.jpg": "e84692154bd077a65b0b951d595231c8",
-"assets/assets/wordpress.jpg": "4c5c0daf4e65ed3c6b4aace9fbc7ffbb",
-"assets/assets/works/kiwix.png": "286e0afb2cfc047115f2293d9c1efba4",
-"assets/assets/works/badgemagic.png": "1068aac879d03cfb8ad0026513d08ffe",
-"assets/assets/works/vibe.jpg": "c1bcc9cba92987b3b11825e7fa789a44",
-"assets/assets/works/templeapp.png": "feb56ae7e6f70143983d0c8f818ac3a0",
-"assets/assets/works/gitgo.png": "32f702f4a59a35a862bea0fd92abda1b",
-"assets/assets/works/tweegenous.png": "587e54ba3904dee07a0455c458c26123",
-"assets/assets/works/hand.png": "baca024caaf49f67add194e6ba12fa9b",
-"assets/assets/works/dexify.png": "363dc735100f6160160919db2cc26879",
-"assets/assets/works/computervision.jpg": "e84692154bd077a65b0b951d595231c8",
-"assets/assets/works/mifos.png": "7f85d2e0715ff102b75319947a36c2d5",
-"assets/assets/works/hm.jpg": "ace2acfc3336947826f9258218bb7776",
-"assets/assets/works/emotion.png": "8ae362cdf0050fd04a69b2ca5a967fc0",
-"assets/assets/works/arpaint.png": "fa407caa93144c0e79cbb11ca17315d4",
-"assets/assets/works/computer-vision.jpg": "fc2ca7439f64bd773391df43792c6546",
-"assets/assets/works/cctv.jpeg": "de11e7687662f53e16ada84f7def5a38",
-"assets/assets/works/ss.png": "00c0d0c66c950fe7cd389f1d23a8745f",
-"assets/assets/works/phimpme.png": "766dff8e142e26f4e67eda3fbf1d2fb8",
-"assets/assets/works/commonsapp.png": "f8d98254690e21cb7abf58b610dd3ff6",
-"assets/assets/works/covid.jpg": "cc9625a5cdbe14b0b74d42eed54945cc",
-"assets/assets/works/hh.jpeg": "e91c71131bae61f5516177a451c60a9c",
-"assets/assets/works/image.jpeg": "e5187aaebabff8e030d560f3827884fa",
-"assets/assets/works/asha.jpg": "df6d84677e8bf157307175895e709545",
-"assets/assets/works/cms.png": "60fae13591d3805db2fcebd44088c203",
-"assets/assets/works/mentored.png": "d5fdc246e873b44dce2905d9fcb37f0b",
-"assets/assets/works/navft.jpeg": "61b4627186b3cc2ed686e1e5eade8af9",
-"assets/assets/works/kitn.png": "8d8c9a80ef13dca17e40c155035810eb",
-"assets/assets/works/lorax.png": "5285884a09bc02e8a01e6639edebdff7",
 "assets/assets/moon.png": "a270b8a10d1a9a52441bf5a322dd1b86",
-"assets/assets/instagram.png": "26631a4043b14dff84180bdf51c3cacb",
-"assets/assets/FontManifest.json": "59c37979205b4b43589051e92e27cbcd",
+"assets/assets/avatar.jpg": "da20d77ff86d7105a0b33f8fa44361bb",
+"assets/assets/wordpress.jpg": "4c5c0daf4e65ed3c6b4aace9fbc7ffbb",
 "assets/assets/GoogleSansRegular.ttf": "b5c77a6aed75cdad9489effd0d5ea411",
+"assets/assets/works/phimpme.png": "766dff8e142e26f4e67eda3fbf1d2fb8",
+"assets/assets/works/gitgo.png": "32f702f4a59a35a862bea0fd92abda1b",
+"assets/assets/works/kitn.png": "8d8c9a80ef13dca17e40c155035810eb",
+"assets/assets/works/mentored.png": "d5fdc246e873b44dce2905d9fcb37f0b",
+"assets/assets/works/badgemagic.png": "1068aac879d03cfb8ad0026513d08ffe",
+"assets/assets/works/navft.jpeg": "61b4627186b3cc2ed686e1e5eade8af9",
+"assets/assets/works/vit.jpeg": "0ef1c5658d67e7d7087d88b1242821f8",
+"assets/assets/works/kiwix.png": "286e0afb2cfc047115f2293d9c1efba4",
+"assets/assets/works/commonsapp.png": "f8d98254690e21cb7abf58b610dd3ff6",
+"assets/assets/works/hm.jpg": "ace2acfc3336947826f9258218bb7776",
+"assets/assets/works/arpaint.png": "fa407caa93144c0e79cbb11ca17315d4",
+"assets/assets/works/hand.png": "baca024caaf49f67add194e6ba12fa9b",
+"assets/assets/works/lorax.png": "5285884a09bc02e8a01e6639edebdff7",
+"assets/assets/works/asha.jpg": "df6d84677e8bf157307175895e709545",
+"assets/assets/works/ss.png": "00c0d0c66c950fe7cd389f1d23a8745f",
+"assets/assets/works/eus.jpeg": "591d2d76e56bf6ddb136222481c15889",
+"assets/assets/works/computervision.jpg": "e84692154bd077a65b0b951d595231c8",
+"assets/assets/works/image.jpeg": "e5187aaebabff8e030d560f3827884fa",
+"assets/assets/works/mifos.png": "7f85d2e0715ff102b75319947a36c2d5",
+"assets/assets/works/cctv.jpeg": "de11e7687662f53e16ada84f7def5a38",
+"assets/assets/works/covid.jpg": "cc9625a5cdbe14b0b74d42eed54945cc",
+"assets/assets/works/cms.png": "60fae13591d3805db2fcebd44088c203",
+"assets/assets/works/computer-vision.jpg": "fc2ca7439f64bd773391df43792c6546",
+"assets/assets/works/templeapp.png": "feb56ae7e6f70143983d0c8f818ac3a0",
+"assets/assets/works/tweegenous.png": "587e54ba3904dee07a0455c458c26123",
+"assets/assets/works/hh.jpeg": "e91c71131bae61f5516177a451c60a9c",
+"assets/assets/works/bfa.jpeg": "d0c84b70dd947d51cd5a18a06d19675c",
+"assets/assets/works/vibe.jpg": "c1bcc9cba92987b3b11825e7fa789a44",
+"assets/assets/works/dexify.png": "363dc735100f6160160919db2cc26879",
+"assets/assets/works/emotion.png": "8ae362cdf0050fd04a69b2ca5a967fc0",
+"assets/assets/FontManifest.json": "59c37979205b4b43589051e92e27cbcd",
 "assets/assets/blog.png": "1269539dc4954cd7f8a8f75b2de5ac8c",
 "assets/assets/twitter.png": "8f35a40403a84631c4125c4f1859c7a6",
-"assets/assets/avatar.jpg": "da20d77ff86d7105a0b33f8fa44361bb",
-"assets/assets/github.png": "d22ee3727a7216019c3848df6eafa024",
-"version.json": "22373029b8b4e095dcd5f190f596254a"
+"assets/assets/linkedin.png": "926e2dcf5ab4220a359867614556df68"
 };
 
 // The application shell files that are downloaded before a service worker can
@@ -67,7 +70,7 @@ self.addEventListener("install", (event) => {
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
       return cache.addAll(
-        CORE.map((value) => new Request(value + '?revision=' + RESOURCES[value], {'cache': 'reload'})));
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
     })
   );
 });
@@ -193,7 +196,7 @@ async function downloadOffline() {
     }
     currentContent[key] = true;
   }
-  for (var resourceKey in Object.keys(RESOURCES)) {
+  for (var resourceKey of Object.keys(RESOURCES)) {
     if (!currentContent[resourceKey]) {
       resources.push(resourceKey);
     }
